77f3a1f2b4ae5907126fcc71336e7ed0cfbc03bc
Tuan Nam <nttnnam@gmail.com>
2022-09-19 19:51:39 +0700

    practice

--- excercise12.js
+++ excercise12.js
@@ -2,16 +2,15 @@
 For example if you accept 025468 the output should be 0-254-6-8. Go to the editor */
 
 function insertDash(num) {
-  let str = num.toString();
-  let result = str[0];
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] % 2 === 0 && str[i] % 2 === 0) {
-      result += "-" + str[i];
-    } else {
-      result += str[i];
+  let newString = num.toString();
+
+  let stringLength = newString.length;
+
+  for (let i = 0; i < stringLength; i++) {
+    if (newString[i] % 2 === 0 && newString[i + 1] % 2 === 0) {
+      newString = newString.slice(0, i + 1) + "-" + newString.slice(i + 1);
     }
   }
-  return result;
 }
 
 console.log(insertDash(25468));
