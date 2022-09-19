20225b966ddd9661fef021acfb4ed53b8907bfa4
Tuan Nam <nttnnam@gmail.com>
2022-09-19 19:41:45 +0700

    practice

--- excercise12.js
+++ excercise12.js
@@ -1,2 +1,17 @@
 /*  Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. 
 For example if you accept 025468 the output should be 0-254-6-8. Go to the editor */
+
+function insertDash(num) {
+  let str = num.toString();
+  let result = str[0];
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] % 2 === 0 && str[i] % 2 === 0) {
+      result += "-" + str[i];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+console.log(insertDash(25468));
