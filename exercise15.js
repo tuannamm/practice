c0f8e0594f8b5a183fa5c7d9924194c3807ba0c1
Tuan Nam <nttnnam@gmail.com>
2022-09-20 18:24:24 +0700

    update

---
+++ exercise15.js
@@ -0,0 +1,9 @@
+/*
+  Write a JavaScript program to find the most frequent item of an array. Go to the editor
+  Sample array : var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+  Sample Output : a ( 5 times )
+*/
+
+const findMostFrequentItem = (arr) => {
+  if (!Array.isArray(arr)) return false;
+};
